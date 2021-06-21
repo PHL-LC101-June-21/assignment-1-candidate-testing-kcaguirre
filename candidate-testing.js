b3a5649f79cb22b2f312a8e9da677cbbef3ae002
kcaguirre <55071486+kcaguirre@users.noreply.github.com>
2021-06-21 20:10:57 +0000

    Added multiple questions to quiz; Questions, correct answers, candidate actions are arrays; Template for display added

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,9 +8,21 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+  ];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -20,21 +32,28 @@ candidateName = input.question("What is your name? ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
+for (let i = 0; i < questions.length; i++){
+  candidateAnswers[i] = input.question(questions[i]);
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+for (let i = 0; i < questions.length; i++){
+  console.log(i+1 + ") " + questions[i]);
+  console.log("Your Answer: " + candidateAnswers[i]);
+  console.log("Correct Answer: " + correctAnswers[i] + '\n');
+}
 if(candidateAnswer == correctAnswer){
   console.log("Your answer was correct!");
 }
 else console.log("Your answer was incorrect!");
 
-  let grade;
+ // let grade;
   
 
-  return grade;
+//  return grade;
 }
 
 function runProgram() {
